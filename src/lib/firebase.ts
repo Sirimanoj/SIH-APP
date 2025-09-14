ae515daad83221211433154a23cec56967698d93
Siri Manoj <sirimanoj1465@gmail.com>
2025-09-14 15:48:28 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,13 @@ import { initializeApp, getApps } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  "projectId": "studio-9057087504-c0bb3",
+  "appId": "1:564574159159:web:d9eb6097ce9f57babc5ab2",
+  "storageBucket": "studio-9057087504-c0bb3.firebasestorage.app",
+  "apiKey": "AIzaSyDlqPdsfd_Unq4zk-pf_0AJCDdKxDCNuao",
+  "authDomain": "studio-9057087504-c0bb3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "564574159159"
 };
 
 // Initialize Firebase
