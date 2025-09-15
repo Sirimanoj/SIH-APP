290a3dec63f9fb4f1013dfd7d6a8cae7886cf917
Siri Manoj <sirimanoj1465@gmail.com>
2025-09-15 07:06:12 +0000

    this is the error

--- src/app/(app)/chatbot/page.tsx
+++
@@ -1,9 +0,0 @@
-import ChatInterface from '@/components/chatbot/chat-interface';
-
-export default function ChatbotPage() {
-  return (
-    <div className="h-[calc(100vh-10rem)] w-full">
-      <ChatInterface />
-    </div>
-  );
-}
