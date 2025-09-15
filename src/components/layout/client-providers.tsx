cbb327d2051eae2d2295ad0d51adf69cbe8c4a24
Siri Manoj <sirimanoj1465@gmail.com>
2025-09-15 07:13:27 +0000

    pleaase fix this error asap

---
+++ src/components/layout/client-providers.tsx
@@ -0,0 +1,19 @@
+
+'use client';
+
+import React from 'react';
+import { SidebarProvider, SidebarInset } from '@/components/ui/sidebar';
+import { MainNav } from '@/components/layout/main-nav';
+import { Header } from '@/components/layout/header';
+
+export function ClientProviders({ children }: { children: React.ReactNode }) {
+  return (
+    <SidebarProvider>
+      <MainNav />
+      <SidebarInset>
+        <Header />
+        <main className="flex-1 p-4 md:p-6 lg:p-8">{children}</main>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
