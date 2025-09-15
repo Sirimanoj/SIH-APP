7b7ae55e8ba8637a6273d7843f6b09dfedd65b11
Siri Manoj <sirimanoj1465@gmail.com>
2025-09-15 05:33:59 +0000

    Try fixing this error: `Runtime Error: Error: useAuth must be used withi

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -4,11 +4,9 @@ import React from 'react';
 import { SidebarProvider, SidebarInset } from '@/components/ui/sidebar';
 import { ClientNav } from '@/components/layout/client-nav';
 import { Header } from '@/components/layout/header';
-import { AuthProvider } from '@/hooks/use-auth';
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <AuthProvider>
       <SidebarProvider>
         <ClientNav />
         <SidebarInset>
@@ -16,6 +14,5 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
           <main className="flex-1 p-4 md:p-6 lg:p-8">{children}</main>
         </SidebarInset>
       </SidebarProvider>
-    </AuthProvider>
   );
 }
